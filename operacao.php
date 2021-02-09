fee70fae45233b162c84de7c570b00c470da62f0
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-02-09 16:01:45 -0300

    operações

---
+++ operacao.php
@@ -0,0 +1,20 @@
+<?php
+$idade = 25;
+$idadeDaqui10Anos = $idade + 10;
+echo "Idade + 10 anos: {$idadeDaqui10Anos} anos\n";
+
+// Operadores
+
+$soma = 2 + 2;
+$subtracao = 2-2;
+$multiplicao = 2 * 2;
+$divisao = 10 / 2;
+$aocubo = 2** 3;
+$restodiv = 2 % 3;
+
+echo"Soma: {$soma}, 
+Subtração: {$subtracao},
+Multiplicação:{$multiplicao}, 
+Divisão: {$divisao}, 
+Aocubo: {$aocubo}, 
+Resto: {$restodiv}";
